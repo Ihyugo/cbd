38afb6e85ae22a6cccb89a02a12c7a5054b3db06
yugo ikuta <yugo1997.10.14@gmail.com>
2017-12-02 12:19:00 +0900

    new3

--- sample.c
+++ sample.c
@@ -4,11 +4,8 @@ int main(void){
 	double sum=0.0,total=0.0;
 	for(i=0;i<100000000;i++){
 		sum = (2*i + 1);
-		if(i % 2 == 0){
-		total += 1/sum;
-		}else{
-		total -= 1/sum;
-		}
+		if(i % 2 == 0) 	total += 1/sum;
+		else 	total -= 1/sum;
 	}
 		printf("%f\n" ,4*total);
 }
--- sample.java
+++ sample.java
@@ -4,11 +4,8 @@ public class sample{
 		double sum,total=0.0;
 		for(i=0; i<100000000; i++){
 			sum = 2*i + 1;
-			if(i%2 == 0){
-				total += 1/sum;
-			}else{
-				total -= 1/sum;
-			}
+			if(i%2 == 0)	total += 1/sum;
+			else	total -= 1/sum;
 		}
 		System.out.println(4*total);
 	}
